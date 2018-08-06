033e3efa6756643f5803fdf45f2498c72ebae513
Хамелеон <maxali946@gmail.com>
2018-08-06 23:59:46 +0400

    Add project files.

---
+++ TwitterClientServerMVVMBD/Common/NotifiableObject.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TwitterClientServerMVVMBD.Common
+{
+    public class NotifiableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ TwitterClientServerMVVMBD/Common/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TwitterClientServerMVVMBD.Common
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute,
+            Predicate<object> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute?.Invoke(parameter) ?? true;
+        }
+        public void Execute(object parameter)
+        {
+            this.execute.Invoke(parameter);
+        }
+    }
+}
